bf15fec15aca7bca395aa2df247ec26a1e900508
FannySivhed <fanny.sivhed@hotmail.com>
2021-04-28 17:34:54 +0200

    ta bort, snart klart

--- static/script.js
+++ static/script.js
@@ -5,7 +5,7 @@ $(document).ready(function() {
 		var old_password = prompt("Ange ditt gamla lösenord");
         var new_password = prompt("Ange ditt nya lösenord");
         // Ändrar lösenordet
-
+console.log("hej")
     $("#delete").on("click", function() {
         // Frågar användaren efter vilket inlägg som ska bort
         var remove = prompt("Ange titeln på det inlägg som du vill ta bort");

627fc948704f1ba039066aaa91c367ab23ae7bd4
FannySivhed <fanny.sivhed@hotmail.com>
2021-05-17 20:06:45 +0200

    soretera

--- project.py
+++ project.py
@@ -183,14 +183,28 @@ def posts():
 @route('/posts', method='POST') #annars lägg till som def och skapa en html
 def posts_category():
     category = getattr(request.forms, 'category')
-    cursor.execute("SELECT * FROM Recipes WHERE Categories = '%s'" + category)
-    recipes = cursor.fetchall()
+    if category == "Kategori":
+            cursor.execute('SELECT Picture, Recipeid, Title FROM Recipes')
+            recipes = cursor.fetchall()
 
-    recipe_list = []
+            recipe_list = []
 
-    for r in recipes:
-        recipe_dict = {'id': r[1], 'img': r[0], 'title': r[2]}
-        recipe_list.append(recipe_dict)
+            #Lexikon
+            for r in recipes:
+                recipe_dict = {'id': r[1], 'img': r[0], 'title': r[2]}
+                recipe_list.append(recipe_dict)
+    else: 
+        sql = "SELECT * FROM Recipes WHERE Categories = %s"
+        adr = (category, )
+        cursor.execute(sql,adr)
+        recipes = cursor.fetchall()
+
+
+        recipe_list = []
+
+        for r in recipes:
+            recipe_dict = {'id': r[1], 'img': r[0], 'title': r[2]}
+            recipe_list.append(recipe_dict)
 
     return template('posts', recipes = recipe_list)
 

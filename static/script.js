ef450e977ddbb09f8409be7d3adcf6737ddc5c70
unknown <81237771+Burreh@users.noreply.github.com>
2021-05-20 16:27:04 +0200

    Klar med navbar

--- static/script.js
+++ static/script.js
@@ -28,29 +28,11 @@ $('.bi').click(function(){
 ;
 
 // Navbar
-function navSlide() {
-  const burger = document.querySelector(".burger");
-  const nav = document.querySelector(".nav-links");
-  const navLinks = document.querySelectorAll(".nav-links li");
-  
-  burger.addEventListener("click", () => {
-      //Toggle Nav
-      nav.classList.toggle("nav-active");
-      
-      //Animate Links
-      navLinks.forEach((link, index) => {
-          if (link.style.animation) {
-              link.style.animation = ""
-          } else {
-              link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.5}s`;
-          }
-      });
-      //Burger Animation
-      burger.classList.toggle("toggle");
-  });
-  
-}
+const hamburger = document.getElementsByClassName('hamburger')[0]
+const navbarLinks = document.getElementsByClassName('navbar-links')[0]
 
-navSlide();
+hamburger.addEventListener('click', () => {
+    navbarLinks.classList.toggle('active')
+})
 
 //KD3

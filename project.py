936081c9fcbd39ffc6ce865769ce58bc9404fa33
josefinatranucharat <josefinatranucharat@gmail.com>
2021-05-22 21:24:40 +0200

    Kommentera bort icke fungerande sak

--- project.py
+++ project.py
@@ -298,9 +298,9 @@ def save_to_database(session):
     sql = 'INSERT INTO Recipes(Title, Portion, Ingredients, Instructions, Picture, Email) VALUES (%s, %s, %s, %s, %s, %s)'
     val = (title, portions, ingredients, instructions, '/static/' + picture.filename, session['username'])
     cursor.execute(sql, val)
-    cursor.execute(f"select Recipeid from Recipes where Title= '{title}'")
-    id= cursor.fetchall()
-    cursor.execute('INSERT INTO Tags(Categories, Recipeid) VALUES (%s,%s)' % category, id)
+    #cursor.execute(f"select Recipeid from Recipes where Title= '{title}'")
+    #id= cursor.fetchall()
+    #cursor.execute('INSERT INTO Tags(Categories, Recipeid) VALUES (%s,%s)' % category, id)
     foodnestdb.commit()
 
     return redirect('posts')
